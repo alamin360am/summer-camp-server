174aad89058e4b5eb4542f22a92e2e71d5ea626a
alamin360am <alaminhimu0@gmail.com>
2023-06-13 17:59:29 +0600

    card query with email done

--- index.js
+++ index.js
@@ -52,7 +52,15 @@ async function run() {
 
     // Cart API
 
-    
+    app.get('/carts', async(req, res)=>{
+      const email = req.query.email;
+      if(!email) {
+        res.send([])
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
 
     app.post('/carts', async(req, res) =>{
       const item = req.body;
